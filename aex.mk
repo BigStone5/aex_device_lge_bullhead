d1b39ae40098ee0ba757a440b6c887bbc3abffd8
Oleg <olegano.bigstone@gmail.com>
2018-04-08 15:23:16 +1000

    Initial commit

---
+++ aex.mk
@@ -0,0 +1,23 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit some common AospExtended stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := AEX_bullhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5X
+TARGET_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM2.171019.029 4657601 release-keys"
+
+BUILD_FINGERPRINT := google/bullhead/bullhead:8.1.0/OPM2.171019.029/4657601:user/release-keys
